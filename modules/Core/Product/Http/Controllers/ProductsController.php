274382a472c092b677759c5941dd8e84040b7fc3
Peter Clayder <peterclayder@gmail.com>
2019-05-30 22:46:32 -0300

    [added] Estrutura do controller Categories

---
+++ modules/Core/Product/Http/Controllers/ProductsController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Modules\Core\Product\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class ProductsController extends Controller
+{
+    public function index(){
+        echo "oi";
+    }
+}
