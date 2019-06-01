2460170117c4670ca04886a1cdf8b4cea20f7744
Peter Clayder <peterclayder@gmail.com>
2019-06-01 16:27:01 -0300

    [removed] Routes Web

--- tresle/Product/Http/Controllers/CategoriesController.php
+++ tresle/Product/Http/Controllers/CategoriesController.php
@@ -3,10 +3,11 @@
 namespace Tresle\Product\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Tresle\Product\Model\Categories;
 
 class ProductsController extends Controller
 {
     public function index(){
-        echo "oi";
+        return Categories::all();
     }
 }
