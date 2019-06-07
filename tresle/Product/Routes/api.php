6e0dd7a5e109b184cdee8a2f388163ef511c8d89
Peter Clayder <peterclayder@gmail.com>
2019-06-06 23:28:22 -0300

    [added] Route api/v1/product/additional/category

--- tresle/Product/Routes/api.php
+++ tresle/Product/Routes/api.php
@@ -7,3 +7,11 @@ Route::prefix("api/v1/product/")->group(function() {
     Route::resource("category", "Product\CategoryController");
     Route::get("category/q/{name}", "Product\CategoryController@search");
 });
+
+/**
+ * Categoria dos produtos adicionais
+ */
+Route::prefix("api/v1/product/additional/")->group(function() {
+    Route::resource("category", "Additional\CategoryController");
+    Route::get("category/q/{name}", "Additional\CategoryController@search");
+});
