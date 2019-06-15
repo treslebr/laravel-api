3981ef5c1ab21b545e51c2d065493d8fe212d1a5
Peter Clayder <peterclayder@gmail.com>
2019-06-15 19:32:41 -0300

    :heavy_plus_sign: (product) : Product controller
    Criei a estrutura do ProductController
    
    issues: #8

---
+++ tresle/Product/Http/Controllers/Product/ProductController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tresle\Product\Http\Controllers\Product;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Tresle\Product\Model\Product\Product;
+
+class ProductController extends Controller
+{
+
+}
