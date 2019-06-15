00ffd0f542776fc7315fb73c65c8a5c25ccfdb56
Peter Clayder <peterclayder@gmail.com>
2019-06-15 19:54:16 -0300

    :heavy_plus_sign: (product) : Product Controller
    issues: #8

--- tresle/Product/Http/Controllers/Product/ProductController.php
+++ tresle/Product/Http/Controllers/Product/ProductController.php
@@ -4,9 +4,18 @@ namespace Tresle\Product\Http\Controllers\Product;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
 use Tresle\Product\Model\Product\Product;
 
 class ProductController extends Controller
 {
-
+    /**
+     * @param Request $request
+     * @return mixed
+     */
+    public function store(Request $request)
+    {
+//        $data = $request->all();
+//        return Product::create($data);
+    }
 }
