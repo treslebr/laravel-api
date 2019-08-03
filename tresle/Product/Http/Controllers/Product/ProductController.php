f35834d1e7e0cc0d807fdd6c0a2b693dc57b387e
Peter Clayder <peterclayder@gmail.com>
2019-08-03 10:52:21 -0300

    :heavy_plus_sign: (Product) Inserindo adicional a um produto
    Issues: #8

--- tresle/Product/Http/Controllers/Product/ProductController.php
+++ tresle/Product/Http/Controllers/Product/ProductController.php
@@ -20,6 +20,31 @@ class ProductController extends Controller
     }
 
     /**
+     * @param Request $request
+     * @param int $idProduct
+     * @return mixed
+     */
+    public function addAdditionalInProductById(Request $request, $idProduct){
+        $idProduct  = (int)$idProduct;
+        try {
+            /** @var \Tresle\Product\Model\Product\Product $product */
+            $product        = Product::with("additionals")->findOrFail($idProduct);
+            $additionals    = $product->additionals()->select(["product_additional_id"])->get()->toArray();
+            $additionalsId  = $this->filterAdditionalsId($additionals, $request->input("additionalsId"));
+
+            $product->additionals()->attach($additionalsId);
+
+            return ["error" => false, "message" => "", "data" => $product];
+        } catch (ModelNotFoundException $e) {
+            return ["error" => true, "message" => "Produto não encontrado"];
+        }catch (\Illuminate\Database\QueryException $e) {
+            $mensagem = "Erro ao cadastrar o produto adicional";
+            $message = strpos($e->getMessage(), "a foreign key constraint fails") ? "{$mensagem}: Adicional não encontrado" : $mensagem;
+            return ["error" => true, "message" => $message];
+        }
+    }
+
+    /**
      * @return \Illuminate\Database\Eloquent\Builder[]|\Illuminate\Database\Eloquent\Collection|Product[]
      */
     public function index()
