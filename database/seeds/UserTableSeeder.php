546a80df247c2027927cf5873049e3ac6bc6d70d
Fernanda Pires <fpires@id.uff.br>
2019-08-07 22:54:13 -0300

    :heavy_plus_sign: (Authenticated) Seeder User
    Issues: #15

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(\App\User::class)->create([
+            'email' => "peterclayder@gmail.com"
+        ]);
+
+        factory(\App\User::class)->create([
+            'email' => "fpires@id.uff.br"
+        ]);
+    }
+}
