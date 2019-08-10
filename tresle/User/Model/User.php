9feb218c2b4c2782489715209318f1c6acafb2a6
Fernanda Pires <fpires@id.uff.br>
2019-08-10 17:18:48 -0300

    :heavy_plus_sign: (User) Não pode inserir is_admin via cadastro
    Issues: #15

--- tresle/User/Model/User.php
+++ tresle/User/Model/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'is_admin', 'telephone', 'cellphone',
+        'name', 'email', 'password', 'telephone', 'cellphone',
     ];
 
     /**
