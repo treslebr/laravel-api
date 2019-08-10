ceea188a417cf2f151c64cdf525c0550c6d75065
Fernanda Pires <fpires@id.uff.br>
2019-08-10 16:45:19 -0300

    :heavy_plus_sign: (User) Realizando login do usuário
    Issues: #15

--- tresle/User/Http/Auth/AuthController.php
+++ tresle/User/Http/Auth/AuthController.php
@@ -4,8 +4,10 @@
 namespace Tresle\User\Http\Auth;
 
 
+use Carbon\Carbon;
 use Tresle\User\Model\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends \App\Http\Controllers\Controller
 {
@@ -31,5 +33,36 @@ class AuthController extends \App\Http\Controllers\Controller
         return response()->json([
             'message' => 'Successfully created user!'
         ], 201);
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+            'remember_me' => 'boolean'
+        ]);
+        $credentials = request(['email', 'password']);
+        if(!Auth::attempt($credentials))
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        $user = $request->user();
+        $tokenResult = $user->createToken('Personal Access Token');
+        $token = $tokenResult->token;
+        if ($request->remember_me)
+            $token->expires_at = Carbon::now()->addWeeks(1);
+        $token->save();
+        return response()->json([
+            'access_token' => $tokenResult->accessToken,
+            'token_type' => 'Bearer',
+            'expires_at' => Carbon::parse(
+                $tokenResult->token->expires_at
+            )->toDateTimeString()
+        ]);
     }
 }
--- tresle/User/Routes/api.php
+++ tresle/User/Routes/api.php
@@ -4,4 +4,5 @@ $version = "v1";
 
 Route::prefix("api/{$version}/user")->group(function() {
     Route::post("/", "\Tresle\User\Http\Auth\AuthController@store");
+    Route::post("/login", "\Tresle\User\Http\Auth\AuthController@login");
 });
