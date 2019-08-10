cf476aa7bc85ca3788b0fe3602b7a332bec8ba5e
Fernanda Pires <fpires@id.uff.br>
2019-08-10 17:29:55 -0300

    :heavy_plus_sign: (User) Para cadastrar um usuário, tem que ser admin e estar logado.
    Issues: #15

--- tresle/User/Routes/api.php
+++ tresle/User/Routes/api.php
@@ -3,12 +3,12 @@
 $version = "v1";
 
 Route::prefix("api/{$version}/user")->group(function() {
-    Route::post("/", "\Tresle\User\Http\Auth\AuthController@store");
     Route::post("/login", "\Tresle\User\Http\Auth\AuthController@login");
 
     Route::group([
-        'middleware' => ['auth:api']
+        'middleware' => ['auth:api','admin']
     ], function() {
+        Route::post("/", "\Tresle\User\Http\Auth\AuthController@store");
         Route::get('/logout', '\Tresle\User\Http\Auth\AuthController@logout');
         Route::get('/logged', '\Tresle\User\Http\Auth\AuthController@getUserLogged');
     });
