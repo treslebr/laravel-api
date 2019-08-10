90a21a5872e098545dc09925ab71f9eaf380eca1
Fernanda Pires <fpires@id.uff.br>
2019-08-10 16:38:07 -0300

    :heavy_plus_sign: (User) Cadastro de usuários
    Issues: #15

--- tresle/User/Http/Auth/AuthController.php
+++ tresle/User/Http/Auth/AuthController.php
@@ -4,7 +4,32 @@
 namespace Tresle\User\Http\Auth;
 
 
+use Tresle\User\Model\User;
+use Illuminate\Http\Request;
+
 class AuthController extends \App\Http\Controllers\Controller
 {
-
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request){
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|confirmed',
+            'telephone' => 'required|string',
+        ]);
+        $user = new User([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            'telephone' => $request->telephone,
+            'cellphone' => $request->cellphone,
+        ]);
+        $user->save();
+        return response()->json([
+            'message' => 'Successfully created user!'
+        ], 201);
+    }
 }
--- tresle/User/Routes/api.php
+++ tresle/User/Routes/api.php
@@ -1,1 +1,7 @@
 <?php
+
+$version = "v1";
+
+Route::prefix("api/{$version}/user")->group(function() {
+    Route::post("/", "\Tresle\User\Http\Auth\AuthController@store");
+});
