ba5373f379e50f2e0587dcfb67cc73eb2495414f
Fernanda Pires <fpires@id.uff.br>
2019-08-11 14:25:43 -0300

    :heavy_plus_sign: (Customer) Login, Logout e retornando cliente logado.
    Issues: #17

--- tresle/Customer/Routes/api.php
+++ tresle/Customer/Routes/api.php
@@ -4,4 +4,12 @@ $version = "v1";
 
 Route::prefix("api/{$version}/customer")->group(function() {
     Route::post("/", "\Tresle\Customer\Http\CustomerController@store");
+    Route::post("/login", "\Tresle\User\Http\Auth\AuthController@login");
+
+    Route::group([
+        'middleware' => ['auth:api']
+    ], function() {
+        Route::get('/logout', '\Tresle\User\Http\Auth\AuthController@logout');
+        Route::get('/logged', '\Tresle\User\Http\Auth\AuthController@getUserLogged');
+    });
 });
