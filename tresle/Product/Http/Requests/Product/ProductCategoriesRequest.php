14d554c0d88204d91074a7854f1a386d68d441f3
Peter Clayder <peterclayder@gmail.com>
2019-08-15 02:07:14 -0300

    :heavy_plus_sign: (Product Category) Route PATCH
    Issues: #21

--- tresle/Product/Http/Requests/Product/ProductCategoriesRequest.php
+++ tresle/Product/Http/Requests/Product/ProductCategoriesRequest.php
@@ -24,14 +24,17 @@ class ProductCategoriesRequest extends FormRequest
     public function rules()
     {
         $rules = [
-            'name'                => "required|min:2|max:190|"
+            'name'                => "min:2|max:190|"
         ];
         switch($this->method()) {
             case "POST": // CRIAÇÃO DE UM NOVO REGISTRO
-                $rules["name"]    .= "|unique:product_categories";
+                $rules["name"]    .= "|required|unique:product_categories";
                 break;
             case "PUT": // ATUALIZAÇÃO DE UM REGISTRO EXISTENTE
-                $rules["name"]    .= "|unique:product_categories,name,".$this->category;
+                $rules["name"]    .= "|required|unique:product_categories,name,".$this->category;
+                break;
+            case "PATCH":
+                $rules["name"]    .= "|filled|unique:product_categories,name,".$this->category;
                 break;
             default:break;
         }
