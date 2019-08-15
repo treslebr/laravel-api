07ad44f3bb3f601cdb5e91817bd4e5fb9aafc7fa
Peter Clayder <peterclayder@gmail.com>
2019-08-15 01:54:58 -0300

    :heavy_plus_sign: (Product Additional Category) Route PATCH
    Issues: #21

--- tresle/Product/Http/Requests/Additional/AdditionalCategoryRequest.php
+++ tresle/Product/Http/Requests/Additional/AdditionalCategoryRequest.php
@@ -24,14 +24,17 @@ class AdditionalCategoryRequest extends FormRequest
     public function rules()
     {
         $rules = [
-            'name'                => "required|min:2|max:190|"
+            'name'                => "min:2|max:190|"
         ];
         switch($this->method()) {
             case "POST": // CRIAÇÃO DE UM NOVO REGISTRO
-                $rules["name"]    .= "|unique:product_additional_category";
+                $rules["name"]    .= "|required|unique:product_additional_category";
                 break;
             case "PUT": // ATUALIZAÇÃO DE UM REGISTRO EXISTENTE
-                $rules["name"]    .= "|unique:product_additional_category,name,".$this->category;
+                $rules["name"]    .= "|required|unique:product_additional_category,name,".$this->category;
+                break;
+            case "PATCH": // ATUALIZAÇÃO DE UM REGISTRO EXISTENTE
+                $rules["name"]    .= "|filled|unique:product_additional_category,name,".$this->category;
                 break;
             default:break;
         }
