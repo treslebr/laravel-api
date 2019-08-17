c3398fcb1ca7e9b4d5695d76647b1b770fd307b2
Peter Clayder <peterclayder@gmail.com>
2019-08-17 13:02:31 -0300

    :heavy_plus_sign: (Cart) Rota para inserir item no carrinho.
    Issues: #25

--- tresle/Cart/Routes/api.php
+++ tresle/Cart/Routes/api.php
@@ -1,1 +1,12 @@
 <?php
+
+
+$version = "v1";
+
+Route::prefix("api/{$version}/cart")->group(function() {
+    Route::group([
+        'middleware' => ['auth:api', 'authCustomer']
+    ], function() {
+        Route::post('/', '\Tresle\Cart\Http\Controllers\CartController@store');
+    });
+});
