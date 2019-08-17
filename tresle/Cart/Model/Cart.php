d7361e2efc51962435993f249aeb8aa82e7d1cc5
Peter Clayder <peterclayder@gmail.com>
2019-08-17 13:05:25 -0300

    :heavy_plus_sign: (Cart) Model Cart.
    Issues: #25

---
+++ tresle/Cart/Model/Cart.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace Tresle\Cart\Model;
+
+
+use Illuminate\Database\Eloquent\Model;
+use Tresle\Cart\Http\Requests\CartRequest;
+use Tresle\Product\Model\Product\Product;
+
+class Cart extends Model
+{
+    /**
+     * @var string
+     */
+    protected $table = "cart";
+
+    /**
+     * @var array
+     */
+    protected $fillable = ["customer_id", "qty", "obs", "product_id", "additionals_id"];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function customer()
+    {
+        return $this->belongsTo(
+            "\Tresle\Customer\Model\Customer\Customer",
+            "customer_id"
+        );
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function product()
+    {
+        return $this->belongsTo(
+            "\Tresle\Product\Model\Product\Product",
+            "product_id"
+        );
+    }
+
+}
